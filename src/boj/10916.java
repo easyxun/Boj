7748e0becb2881106ac48e3d01fb152d3cead9f9
easyxun <easyxun@gmail.com>
2024-05-01 13:24:59 +0900

    10916

---
+++ src/boj/10916.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int result = (int)Math.pow(2, n);
+        System.out.println(result);
+    }
+}
