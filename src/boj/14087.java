3d3c917fed14aaccb5a39e5d8cc86d005397c7d2
easyxun <easyxun@gmail.com>
2024-05-27 13:25:02 +0900

    14087

---
+++ src/boj/14087.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int gcd = gcd(n, m);
+        int lcm = n * m / gcd;
+        
+        System.out.println(gcd + " " + lcm);
+        
+        sc.close();
+    }
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
