3263f79dc1c102e30ec344bea7b613c101055f90
easyxun <easyxun@gmail.com>
2024-06-13 13:18:57 +0900

    1937

---
+++ src/boj/1937.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        map = new int[n][n];
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result = Math.max(result, dfs(i, j));
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static int dfs(int x, int y) {
+        if (dp[x][y] != 0) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                continue;
+            }
+
+            if (map[nx][ny] > map[x][y]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            }
+        }
+
+        return dp[x][y];
+    }
+}
