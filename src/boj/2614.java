a6460305262f1a3a27dfe5e560967954fc9fd85a
easyxun <easyxun@gmail.com>
2024-06-14 13:18:59 +0900

    2614

---
+++ src/boj/2614.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int result = n / m;
+        int remain = n % m;
+        
+        System.out.println(result);
+        System.out.println(remain);
+        
+        sc.close();
+    }
+}
