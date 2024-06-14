1264a89a57bb6014dc0bd4166a7a1c02ee336ce9
easyxun <easyxun@gmail.com>
2024-06-14 13:18:59 +0900

    8402

---
+++ src/boj/8402.java
@@ -0,0 +1,27 @@
+```java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, arr[i] + n - i);
+        }
+        
+        System.out.println(max);
+        
+        sc.close();
+    }
+}
+```
