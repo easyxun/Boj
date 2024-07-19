f4ccdedabbbf88ca92db2aed44ea74eb28522886
easyxun <easyxun@gmail.com>
2024-07-19 13:20:13 +0900

    9336

---
+++ src/boj/9336.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int result = 0;
+            
+            for (int j = 1; j <= N; j++) {
+                result += j % K;
+            }
+            
+            System.out.println(result);
+        }
+        
+        sc.close();
+    }
+}
