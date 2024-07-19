a1045a462b641453e48271d91874888b0ed67164
easyxun <easyxun@gmail.com>
2024-07-19 13:20:13 +0900

    8362

---
+++ src/boj/8362.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        while (k > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = (n - 1) / 2;
+            }
+            k--;
+        }
+
+        System.out.println(n);
+    }
+}
