38c048eac4f7aa0f957843713cc86bb6776e5e9c
easyxun <easyxun@gmail.com>
2024-08-14 13:21:04 +0900

    11125

---
+++ src/boj/11125.java
@@ -0,0 +1,20 @@
+```java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int cnt = 0;
+        while (n > 1) {
+            n /= 2;
+            cnt++;
+        }
+        
+        System.out.println(cnt);
+        
+        sc.close();
+    }
+}
+```
