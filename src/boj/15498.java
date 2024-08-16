c0bae22a0f76ae99c8a2d454984c3545970feb86
easyxun <easyxun@gmail.com>
2024-08-16 13:21:05 +0900

    15498

---
+++ src/boj/15498.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+
+        System.out.println(maxSum);
+    }
+}
