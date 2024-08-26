76a3e9500dccac7a53710827833ff3cc44f4f457
easyxun <easyxun@gmail.com>
2024-08-26 13:21:27 +0900

    10481

---
+++ src/boj/10481.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            int sum = 0;
+
+            for (int j = 0; j < N; j++) {
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }
+
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                if (arr[j] * N > sum) {
+                    cnt++;
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
