40a3ebf0b8b2f897a2bccb383053574a910cfc6e
easyxun <easyxun@gmail.com>
2024-09-05 13:21:41 +0900

    14640

---
+++ src/boj/14640.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        if (N == M) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        
+        sc.close();
+    }
+}
