962f6bf62f1669c53d5cd757e21dd3ac6268c227
easyxun <easyxun@gmail.com>
2024-09-08 13:21:56 +0900

    11214

---
+++ src/boj/11214.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            boolean isPrime = isPrime(n);
+            boolean isPalindrome = isPalindrome(n);
+
+            if (isPrime && isPalindrome) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(int n) {
+        String str = String.valueOf(n);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
