fd8cfb0cb067c4f503d4e47c1b32dd9dc3f19c91
easyxun <easyxun@gmail.com>
2024-10-17 13:07:49 +0900

    1070

---
+++ src/boj/1070.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println(n + 1);
+        } else {
+            System.out.println(n + 2);
+        }
+
+        sc.close();
+    }
+}
