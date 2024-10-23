95c2e6087410bcf0db7c0295b995ce1d2d2814ec
easyxun <easyxun@gmail.com>
2024-10-23 13:08:02 +0900

    10827

---
+++ src/boj/10827.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigDecimal a = new BigDecimal(sc.next());
+        BigDecimal b = new BigDecimal(sc.next());
+        
+        System.out.println(a.pow(b.intValue()));
+        
+        sc.close();
+    }
+}
