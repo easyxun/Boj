3a714eb2f0f03657f85f30c0e93c51ecfa39968f
easyxun <easyxun@gmail.com>
2024-10-26 13:08:09 +0900

    11596

---
+++ src/boj/11596.java
@@ -0,0 +1,16 @@
+```java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("black");
+        } else {
+            System.out.println("white");
+        }
+    }
+}
+```
