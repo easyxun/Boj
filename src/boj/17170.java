67b882d0cedee4e312e3e5d1743f902d8e7c5c6e
easyxun <easyxun@gmail.com>
2024-10-30 13:08:18 +0900

    17170

---
+++ src/boj/17170.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int result = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            if ((n - i) + (m - i) == i) {
+                result++;
+            }
+        }
+        
+        System.out.println(result);
+        
+        sc.close();
+    }
+}
