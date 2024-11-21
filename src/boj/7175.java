2833530ed6124ea71a8f4a45ecc270bc6b5995c6
easyxun <easyxun@gmail.com>
2024-11-21 13:09:07 +0900

    7175

---
+++ src/boj/7175.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == 0) {
+                    System.out.println("YES");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
