e3ac56b156fcfcbfc2acda9d8717e32cbc28298d
easyxun <easyxun@gmail.com>
2024-11-21 13:09:07 +0900

    16877

---
+++ src/boj/16877.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int result = Math.min(n/2, m);
+        
+        System.out.println(result);
+    }
+}
