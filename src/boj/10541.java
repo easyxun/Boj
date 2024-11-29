9d07ec904b7ecd80844844d95f867505c3481cd6
easyxun <easyxun@gmail.com>
2024-11-29 13:09:28 +0900

    10541

---
+++ src/boj/10541.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            int x = sc.nextInt();
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == x) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+
+        sc.close();
+    }
+}
