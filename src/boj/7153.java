2cfd22a2545a86e0ddd5373964608e508e7170c6
easyxun <easyxun@gmail.com>
2024-12-17 13:10:21 +0900

    7153

---
+++ src/boj/7153.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            result += (long)arr1[i] * arr2[i];
+        }
+
+        System.out.println(result);
+    }
+}
