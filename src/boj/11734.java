fe212d25ae2da8a5ad2be3d454006c7848bbe8a0
easyxun <easyxun@gmail.com>
2024-12-30 13:10:33 +0900

    11734

---
+++ src/boj/11734.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            String original = input.replaceAll(" ", "");
+            String translated = original.equals("") ? "¹¹¾ß?" : original;
+
+            System.out.println(translated);
+        }
+
+        sc.close();
+    }
+}
