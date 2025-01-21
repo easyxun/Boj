f3b6a9249ae2e9fc0e467c46362898cdb4db1420
easyxun <easyxun@gmail.com>
2025-01-21 13:11:02 +0900

    15001

---
+++ src/boj/15001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+        
+        long result = n - k + 1;
+        System.out.println(result);
+        
+        sc.close();
+    }
+}
