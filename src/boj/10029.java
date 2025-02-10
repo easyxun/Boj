72b5bd7974ebca6f9ad36601525b85639cfd05a4
easyxun <easyxun@gmail.com>
2025-02-10 13:11:30 +0900

    10029

---
+++ src/boj/10029.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            String color = sc.next();
+            if (color.equals("black") || color.equals("blue")) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
