e41ca6ab2bd3c00ef99a353b0423264d2fa054d1
easyxun <easyxun@gmail.com>
2025-03-01 13:12:19 +0900

    7361

---
+++ src/boj/7361.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i * i * i;
+            }
+            System.out.println(sum);
+        }
+        
+        sc.close();
+    }
+}
