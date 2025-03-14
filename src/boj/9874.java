10294a4d621860ed0c0ac2e0e3e7eacf9d4455c8
easyxun <easyxun@gmail.com>
2025-03-14 13:12:45 +0900

    9874

---
+++ src/boj/9874.java
@@ -0,0 +1,20 @@
+```java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int sum = 0;
+        for (int i = n; i <= m; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
+```
