2d4fde10a309fdbfc733ba4754b5d156c4de7e97
easyxun <easyxun@gmail.com>
2025-03-17 13:12:56 +0900

    13272

---
+++ src/boj/13272.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result += gcd(n, i);
+        }
+
+        System.out.println(result);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
