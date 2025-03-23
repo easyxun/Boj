ac785d5016d8826799dc30db4fb37e1b6cb6145b
easyxun <easyxun@gmail.com>
2025-03-23 13:13:03 +0900

    15511

---
+++ src/boj/15511.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        
+        if (n == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        
+        sc.close();
+    }
+}
