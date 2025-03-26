36eec5a7168c1c02f40ea6a176f58cf1b47deb50
easyxun <easyxun@gmail.com>
2025-03-26 13:13:12 +0900

    14894

---
+++ src/boj/14894.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        
+        long result = n * m - 1;
+        
+        System.out.println(result);
+    }
+}
