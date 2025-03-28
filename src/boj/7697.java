2db5d7d7ce98d9b64a2eec92590bb82bcdb2bef0
easyxun <easyxun@gmail.com>
2025-03-28 13:13:22 +0900

    7697

---
+++ src/boj/7697.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            
+            int count = 0;
+            for (int i = 1; i <= n; i++) {
+                count += i * i;
+            }
+            
+            System.out.println(count);
+        }
+        
+        sc.close();
+    }
+}
